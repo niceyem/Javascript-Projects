f71df478010529854a568d42464077e6d9f1af76
ADISA OPEYEMI <40341509+niceyem@users.noreply.github.com>
2020-05-09 13:54:44 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,131 +1,128 @@
-// var fruits = ['mangoes', 'apples', 'alvocado']
+"use stricts" 
+var fruits = ['mangoes', 'apples', 'alvocado']
 
-// console.log(fruits)
+ console.log(fruits)
 
-// const companies = [
-//     {name: "arbico", category: 'construct', start: 2000},
-//     {name: 'RCC', category: 'construct', start: 1999},
-//     {name: "innovative", category: 'IT', start: 2019},
-//     {name: 'MTN', category: 'Tele', start: 2021}
-// ]
+ const companies = [
+      {name: "arbico", category: 'construct', start: 2000},
+      {name: 'RCC', category: 'construct', start: 1999},
+      {name: "innovative", category: 'IT', start: 2019},
+      {name: 'MTN', category: 'Tele', start: 2021}
+       ]
 
 
-// //     console.log(companies[i]);
-// // }
+    console.log(companies[i]);
+ }
 
-// companies.forEach(function(company) {
-//     console.log(company.name);
-// });
-// const age =[22,11,44,13,55,99,45];
+ companies.forEach(function(company) {
+     console.log(company.name);
+ });
+ const age =[22,11,44,13,55,99,45];
 
-// const candrink =age.filter(age => age <= 66);
+ const candrink =age.filter(age => age <= 66);
 
-// console.log(candrink)
-// "use strict"
+ console.log(candrink)
 
-// let answer = prompt('the official name of javascript')
-// let javas= 'ECMAScript'
+ let answer = prompt('the official name of javascript')
+ let javas= 'ECMAScript'
 
-// if (answer == javas) {
-//     alert('Right')
-// }
+ if (answer == javas) {
+    alert('Right')
+ }
 
-// else {
-//     alert ('Didnt know')
-// }
+ else {
+     alert ('Didnt know')
+ }
 
-// 'use strict'
 
-// let input = prompt('enter number')
+ let input = prompt('enter number')
 
-// if (input == 1){
-//     alert(1)
-// }
-// else if (input == -1) {
-//     alert(-1)
-// }
+ if (input == 1){
+     alert(1)
+ }
+ else if (input == -1) {
+     alert(-1)
+}
 
-// else if (input == 0) {
-//     alert (0)
-// }
+ else if (input == 0) {
+     alert (0)
+ }
 
-// else{
-//     alert('olodo')
-// }
+ else{
+     alert('olodo')
+ }
 
-// 'use strict'
 
-// let she_will = 'YES'
-// let maybe = 'NO'
+ let she_will = 'YES'
+ let maybe = 'NO'
 
-// let propose =prompt('Will you marry me? ')
+let propose =prompt('Will you marry me? ')
 
-// if(propose == she_will){
-//     alert('I LOVE YOU')
-// }
+ if(propose == she_will){
+     alert('I LOVE YOU')
+ }
 
-// else if(propose == maybe){
-//     alert('I wish you well')
-// }
+ else if(propose == maybe){
+     alert('I wish you well')
+ }
 
-// else{
-//     alert('AM AFRAID OF LOSING YOU')
-// }
-"use strict"
+ else{
+     alert('AM AFRAID OF LOSING YOU')
+}
 
-// for(let sum= 1; sum < 10; sum++){
-//     if(sum % 3 == 1){
-//         alert(sum)
-//     }
-// }
+ for(let sum= 1; sum < 10; sum++){
+     if(sum % 3 == 1){
+         alert(sum)
+     }
+ }
 
-// for (let odd= 1; odd <= 10; odd++){
-//     if(odd % 2 == 1){
-//         alert(odd)
-//     }
-// }
+ for (let odd= 1; odd <= 10; odd++){
+    if(odd % 2 == 1){
+        alert(odd)
+    }
+ }
 
-// let i = 0
-// while(i < 3){
-//     alert(`number ${i}`)
-//     i++
-// }
+ let i = 0
+ while(i < 3){
+    alert(`number ${i}`)
+    i++
+ }
 
-// let num =0;
-// do{
-//     num= prompt('enter number', 0)
-// }
-//     while(num <=100 && num)
+ let num =0;
+ do{
+     num= prompt('enter number', 0)
+ }
+     while(num <=100 && num)
     
-// alert(num)
+ alert(num)
 
-// for(let prime= 2; prime <=10; prime++){
-//     if(prime % 2 == 1){
-//         alert(prime)
-//     }
-// }
+ for(let prime= 2; prime <=10; prime++){
+     if(prime % 2 == 1){
+        alert(prime)
+    }
+ }
 
 
 
-// switch (a) {
-//     case '3':
-//         alert('too small')
-//         break;
-//     case '4':
-//         alert('exactly')
-//         break;
-//         case 4:
-//             alert('nice one')
-//             break
-//     default:
-//         alert('i dont know')
-//         break;
-// }
+ switch (a) {
+     case '3':
+         alert('too small')
+        break;
+     case '4':
+         alert('exactly')
+         break;
+        case 4:
+             alert('nice one')
+             break
+     default:
+         alert('i dont know')
+         break;
+ }
 
-// let browser = 'edge'
+ let browser = 'edge'
 
-// if(browser == 'edge'){
-//     alert('you are edge')
+ if(browser == 'edge'){
+     alert('you are edge')
 // }
 // else if(browser == 'chrome' || 'opera' || 'safari'){
 //     alert('ok we support')
